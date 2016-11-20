5efb78a451b6048f30b19a5dd1921f71baaa4abb
Eric Horacek <eric@automatic.com>
2016-11-20 10:45:01 -0800

    Add ValueType lightweight generic to RACSequence
    Continues the pattern established with RACSignal into RACSequence by adding `__coviariant ValueType` lightweight generics.

--- ReactiveObjC/NSDictionary+RACSequenceAdditions.h
+++ ReactiveObjC/NSDictionary+RACSequenceAdditions.h
@@ -8,27 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
-@class RACSequence;
+@class RACSequence<__covariant ValueType>;
+@class RACTuple;
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface NSDictionary (RACSequenceAdditions)
+@interface NSDictionary<__covariant KeyType, __covariant ObjectType> (RACSequenceAdditions)
 
 /// Creates and returns a sequence of RACTuple key/value pairs. The key will be
 /// the first element in the tuple, and the value will be the second.
 ///
 /// Mutating the receiver will not affect the sequence after it's been created.
-@property (nonatomic, copy, readonly) RACSequence *rac_sequence;
+@property (nonatomic, copy, readonly) RACSequence<RACTuple *> *rac_sequence;
 
 /// Creates and returns a sequence corresponding to the keys in the receiver.
 ///
 /// Mutating the receiver will not affect the sequence after it's been created.
-@property (nonatomic, copy, readonly) RACSequence *rac_keySequence;
+@property (nonatomic, copy, readonly) RACSequence<KeyType> *rac_keySequence;
 
 /// Creates and returns a sequence corresponding to the values in the receiver.
 ///
 /// Mutating the receiver will not affect the sequence after it's been created.
-@property (nonatomic, copy, readonly) RACSequence *rac_valueSequence;
+@property (nonatomic, copy, readonly) RACSequence<ObjectType> *rac_valueSequence;
 
 @end
 
