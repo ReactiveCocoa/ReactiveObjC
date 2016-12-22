ce4668a7633ee0ec7f5a14545a66f54a1c0a4f03
Hai Feng Kao <haifeng@cocoaspice.in>
2016-12-22 17:00:40 +0800

    remove the unecessary header guard

--- ReactiveObjC/extobjc/metamacros.h
+++ ReactiveObjC/extobjc/metamacros.h
@@ -6,9 +6,6 @@
  * Released under the MIT license
  */
 
-#ifndef EXTC_METAMACROS_H
-#define EXTC_METAMACROS_H
-
 /**
  * Executes one or more expressions (which may have a void type, such as a call
  * to a function that returns no value) and always returns true.
@@ -662,5 +659,3 @@ metamacro_if_eq(0, 1)(true)(false)
 #define metamacro_drop18(...) metamacro_drop17(metamacro_tail(__VA_ARGS__))
 #define metamacro_drop19(...) metamacro_drop18(metamacro_tail(__VA_ARGS__))
 #define metamacro_drop20(...) metamacro_drop19(metamacro_tail(__VA_ARGS__))
-
-#endif
