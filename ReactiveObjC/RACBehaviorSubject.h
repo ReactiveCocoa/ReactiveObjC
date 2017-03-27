7de7779a7e0d86315b136365a7b405729d1c4988
Erich Grunewald <erich.grunewald@futurice.com>
2017-03-27 18:34:53 +0200

    Add type annotations to RACBehaviorSubject.

--- ReactiveObjC/RACBehaviorSubject.h
+++ ReactiveObjC/RACBehaviorSubject.h
@@ -11,11 +11,11 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /// A behavior subject sends the last value it received when it is subscribed to.
-@interface RACBehaviorSubject : RACSubject
+@interface RACBehaviorSubject<__covariant ValueType> : RACSubject
 
 /// Creates a new behavior subject with a default value. If it hasn't received
 /// any values when it gets subscribed to, it sends the default value.
-+ (instancetype)behaviorSubjectWithDefaultValue:(nullable id)value;
++ (instancetype)behaviorSubjectWithDefaultValue:(nullable ValueType)value;
 
 @end
 
